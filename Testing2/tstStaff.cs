857a6a6997b21b1f78740f36e0f31ca89bddca16
p2725994 <p2725994@my365.dmu.ac.uk>
2024-05-02 15:13:20 +0100

    Added testing for clsStaff

---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an instance of the class we want to create
+            clsStaff aStaff = new clsStaff();
+            //test to see that it exists
+            Assert.IsNotNull(aStaff);
+        }
+    }
+}
