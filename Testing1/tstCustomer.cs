c60d2632d5687890a3236782585a7ee248ea585a
p2721847 <p2721847@my365.dmu.ac.uk>
2024-05-07 15:07:16 +0100

    Added teesting for Customer class (Ahsan)

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance
+            clsCustomer ACustomer = new clsCustomer();
+            //test to see if it exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
