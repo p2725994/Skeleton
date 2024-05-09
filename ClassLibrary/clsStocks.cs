d70fd5b3480e174cf9bdd880dcd7d7a7b4ef02f5
p2761101 <p2761101@GWHXX652WIN012W>
2024-05-09 15:11:00 +0100

    tests updated

--- ClassLibrary/clsStocks.cs
+++ ClassLibrary/clsStocks.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStocks
     {
+        public static DateTime Product_Expiry { get; set; }
+        public static string Product_Description { get; set; }
+        public static string Product_Name { get; set; }
+        public static string Product_Quantity { get; set; }
+        public static Double Product_Price { get; set; }
+        public static int Product_Id { get; set; }
+        public static Boolean Available { get; set; }
     }
 }
