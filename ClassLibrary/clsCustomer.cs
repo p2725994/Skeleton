83ea61992227ea1792bdd8d450993435360a6c54
p2721847 <p2721847@my365.dmu.ac.uk>
2024-05-09 14:46:54 +0100

    changed property names

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -5,11 +5,11 @@ namespace ClassLibrary
     public class clsCustomer
     {
         public bool Verified { get; set; }
-        public DateTime Birthdate { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string EmailAddress { get; set; }
-        public int Phone { get; set; }
-        public int UserID { get; set; }
+        public DateTime CustomerBirthdate { get; set; }
+        public string CustomerFirstname { get; set; }
+        public string CustomerLastname { get; set; }
+        public string CustomerEmail { get; set; }
+        public int CustomerPhone { get; set; }
+        public int CustomerID { get; set; }
     }
 }
