60ec4dfa4a951ee21e66cf98eb25e2e9c25f8803
p2721847 <p2721847@my365.dmu.ac.uk>
2024-05-15 09:10:47 +0100

    redone prac 8

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,22 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of clsCustomer
+        clsCustomer ACustomer = new clsCustomer();
+        //get data from session object
+        ACustomer = (clsCustomer)Session["ACustomer"];
+        //display the firstname for this entry
+        Response.Write(ACustomer.CustomerFirstname);        
+        //display last name for this entry
+        Response.Write(ACustomer.CustomerLastname);
+        //display birthdate for this entry
+        Response.Write(ACustomer.CustomerBirthdate);
+        //display Phone for this entry
+        Response.Write(ACustomer.CustomerPhone);
+        //display email for this entry
+        Response.Write(ACustomer.CustomerEmail);
+
+
 
     }
 }
