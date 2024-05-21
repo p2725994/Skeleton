a6f963f17b95f2a5637829c69d9a934993bbafae
p2725994 <p2725994@my365.dmu.ac.uk>
2024-05-21 20:17:42 +0100

    completed practical 14

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -77,4 +77,74 @@ public partial class _1_List : System.Web.UI.Page
             5.	The error label will display a message to the user if a list item hasn’t been selected.
          */
     }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        //variable to store the PK value of the record to be edited
+        Int32 StaffNo;
+
+        //if a record has been selected from the list
+        if (lstStaffList.SelectedIndex != -1)
+        {
+            //get the PK value of the record to edit
+            StaffNo = Convert.ToInt32(lstStaffList.SelectedValue);
+
+            //store the data in te sesision object
+            Session["StaffNo"] = StaffNo;
+
+            //redirect to the edit page
+            Response.Redirect("StaffConfirmDelete.aspx");
+        }
+        else //if no record has been recorded 
+        {
+            lblError.Text = "Please select a record from the list to delete.";
+        }
+
+    }
+
+    protected void btnApplyFilter_Click(object sender, EventArgs e)
+    {
+        //create an instance of Staff Collection
+        clsStaffCollection AStaff = new clsStaffCollection();
+
+        //retrieve the value of email in the presentation layer
+        AStaff.ReportByEmail(txtFilter.Text);
+
+        //set the data source to the list of emails in the collection
+        lstStaffList.DataSource = AStaff.StaffList;
+
+        //set the name of the PK
+        lstStaffList.DataValueField = "StaffNo";
+
+        //set the name of the field
+        lstStaffList.DataTextField = "StaffEmail";
+
+        //bind the data to the list
+        lstStaffList.DataBind();
+    }
+
+
+    protected void btnClearFilter_Click(object sender, EventArgs e)
+    {
+        //create an instance of Staff Collection
+        clsStaffCollection AStaff = new clsStaffCollection();
+
+        //set an empty string
+        AStaff.ReportByEmail("");
+
+        //clear any existing filter to tidy up the interface
+        txtFilter.Text = "";
+
+        //set the data source to the list of emails in the collection
+        lstStaffList.DataSource = AStaff.StaffList;
+
+        //set the name of the PK
+        lstStaffList.DataValueField = "StaffNo";
+
+        //set the name of the field
+        lstStaffList.DataTextField = "StaffEmail";
+
+        //bind the data to the list
+        lstStaffList.DataBind();
+    }
 }
